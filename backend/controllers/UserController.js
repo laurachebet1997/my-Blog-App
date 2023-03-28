c8d9a2f4a9abb290cfd5837262d97ae7cc37a94d
laurachebet1997 <rosechebet19971997@gmail.com>
2023-03-28 12:09:51 +0300

    cg

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -17,33 +17,44 @@ export const getAllUser = async (req, res, next) => {
 
 export const signUp = async (req,res,next) => {
     const { name, email, password } = req.body
-    
-    let existingUser;
-    try {
-        existingUser = await User.findOne({email})
-    }catch(err) {
-        return console.log(err)
-    }
-    if(existingUser) {
-        return res.status(400).json({msg:"user exists!please login",user:existingUser})
-    }
-    const hashedpassword = bcrypt.hashSync(password)
-    const user = new User({
-        name,
-        email,
-        password: hashedpassword,
-        blogs:[],
-    })
-    
-    try {
-        await user.save()
-    }catch(err){
-        return console.log(err)
-    }
-    return res.status(200).json({user})
 
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('add all fields')
+    }else if (!/^[a-zA-Z]+$/.test(name)) {
+        throw new Error("name must be in letters only")
+    }else if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) {
+        alert("invalid email")
+    }else if (password.length < 6 ) {
+        throw new Error("password must not be less than six characters")
+    }else {
+        let existingUser;
+        try {
+            existingUser = await User.findOne({email})
+        }catch(err) {
+            return console.log(err)
+        }
+        if(existingUser) {
+            return res.status(400).json({msg:"user exists!please login",user:existingUser})
+        }
+        const hashedpassword = bcrypt.hashSync(password)
+        const user = new User({
+            name,
+            email,
+            password: hashedpassword,
+            blogs:[],
+        })
+    
+        try {
+            await user.save()
+         }catch(err){
+            return console.log(err)
+        }
+         return res.status(200).json({user})       
+}
 }
 
+ 
 export const login = async (req,res,next) => {
     const { email, password } = req.body
     let existingUser;
@@ -60,5 +71,9 @@ export const login = async (req,res,next) => {
         res.status(400).json({msg:"password incorrect!"})
     }
     return res.status(200).json({msg:"login successfull",user:existingUser})
+}
+
+export const portfolio = async(req,res,next) => {
+
 }
 
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getAllUser, signUp, login } from "../controllers/UserController.js"
+import { getAllUser, signUp, login, portfolio } from "../controllers/UserController.js"
 
 
 const router = express.Router()
@@ -7,6 +7,7 @@ const router = express.Router()
 router.get('/', getAllUser)
 router.post('/signup', signUp)
 router.post('/login', login)
+router.get('/portfolio',portfolio)
 
 
 export default router;
